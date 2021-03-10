ae2b6224f7a5ca24a9854b0f7aed96c656b8295f
Documenter.jl <documenter@juliadocs.github.io>
2021-03-10 17:19:21 +0000

    build based on b5dff19

---
+++ v0.2.2/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = InstrumentConfig","category":"page"},{"location":"#InstrumentConfig","page":"Home","title":"InstrumentConfig","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [InstrumentConfig]","category":"page"}]
+}
